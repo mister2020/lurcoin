8cdb364a2987469bb8fa489fd505d6cef4b7daa3
Max <doomlate1@gmail.com>
2022-05-11 13:32:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(routes);
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://roxxon.825127-cg63630.tmweb.ru/"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
